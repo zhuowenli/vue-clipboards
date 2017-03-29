bf6c2c0d77404db64cfa96a41e87eb15bbda04db
绿间 <lvjian@meiyaapp.com>
2017-03-29 18:12:00 +0800

    :construction: deploy new build system

---
+++ example/index.js
@@ -0,0 +1,18 @@
+/**
+ * @author: 卓文理
+ * @email : 531840344@qq.com
+ * @desc  : Description
+ */
+
+import Vue from 'vue';
+import 'sweetalert/dist/sweetalert.css';
+
+import App from './App';
+import VueClipboards from '../src/vue-clipboards';
+
+Vue.use(VueClipboards);
+
+export default new Vue({
+    el: '#app',
+    render: h => h(App)
+});

37e2e06fdc00f168752b3ad8d8211631973c5eef
卓文理 <531840344@qq.com>
2017-05-13 17:57:21 +0800

    :bug: fixed #7, multiple registration issues

--- example/App.vue
+++ example/App.vue
@@ -1,7 +1,25 @@
 <template>
     <div id="app">
-        <input type="text" v-model="value">
-        <button v-clipboard="value" @success="handleSuccess" @error="handleError">Copy</button>
+        <div class="input-group">
+            <input class="input-group__inner" type="text" v-model="value">
+            <button
+                class="input-group__append"
+                v-clipboard="value"
+                :key="1"
+                @success="handleSuccess"
+                @error="handleError"
+            >Copy</button>
+        </div>
+        <div class="input-group">
+            <input class="input-group__inner" type="text" v-model="value2">
+            <button
+                class="input-group__append"
+                v-clipboard="value2"
+                :key="2"
+                @success="handleSuccess"
+                @error="handleError"
+            >Copy</button>
+        </div>
     </div>
 </template>
 
@@ -12,42 +30,41 @@
         name: 'App',
         data () {
             return {
-                value: 'Hello world!'
-            }
+                value: 'Hello world!',
+                value2: 'kek'
+            };
         },
         methods: {
-            handleSuccess(e) {
+            handleSuccess (e) {
                 swal(e.text, 'good job, you got it!', 'success');
             },
-            handleError(e) {
+            handleError () {
                 swal('Oops...', 'Something went wrong!', 'error');
-            },
+            }
         }
     };
 </script>
 
-<style>
-#app{
+<style lang="sass">
+.input-group
     display: -webkit-flex;
     display: flex;
     width: 100%;
     max-width: 450px;
-}
-#app input{
-    -webkit-flex: 1;
-    flex: 1;
-    padding: 7px 8px;
-    border: 1px solid #ccc;
-    background: #fff;
-    font-size: 13px;
-    line-height: 13px;
-    outline: 0;
-}
-#app button{
-    display: inline-block;
-    border: 1px solid #ccc;
-    border-left: 0;
-    background-color: #eee;
-    background-image: linear-gradient(#fcfcfc, #eee);
-}
+    margin-bottom: 10px
+    &__inner
+        -webkit-flex: 1;
+        flex: 1;
+        padding: 7px 8px;
+        border: 1px solid #ccc;
+        border-right: 0
+        background: #fff;
+        font-size: 13px;
+        line-height: 13px;
+        outline: 0;
+    &__append
+        display: inline-block;
+        border: 1px solid #ccc;
+        background-color: #eee;
+        background-image: linear-gradient(#fcfcfc, #eee);
 </style>
